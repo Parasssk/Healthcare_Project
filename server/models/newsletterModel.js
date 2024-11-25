74fee555e3b223160087a9ab80a1d70246a167af
Parasssk <paraskapoor4321@gmail.com>
2024-11-25 12:46:11 +0530

    newsletter model added

---
+++ server/models/newsletterModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const newsletterSchema = mongoose.Schema({
+    title:{
+        type : String , 
+        require : [ true , "please add the title"],
+    },
+    author:{
+        type : String , 
+        require : [ true , "please add the author"],
+    },
+    date:{
+        type : String , 
+        require : [ true , "please add the date"],
+    },
+    imageurl:{
+        type : String , 
+        require : [ true , "please add imageUrl"],
+    },
+    description:{
+        type : String , 
+        require : [ true , "please add the description"],
+    }
+},
+{
+    timestamps : true ,
+});
+const newsletter=mongoose.model("Newsletter",newsletterSchema);
+module.exports=newsletter;
